e19aae53e686fa9792e543776d3aa49e75d22075
Edward Webber <github@webbsense.com>
2022-04-05 12:13:38 -0500

    redo products

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
   ],
   imports: [
     BrowserModule
