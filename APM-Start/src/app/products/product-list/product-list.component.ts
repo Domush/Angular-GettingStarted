c70e9ecf96b179b1407c915aa844759b2d86cb7d
Phillip Webber <github@webbsense.com>
2022-04-11 16:09:11 -0500

    Service added

--- APM-Start/src/app/products/product-list/product-list.component.ts
+++ APM-Start/src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
@@ -7,7 +8,7 @@ import { IProduct } from '../product';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  constructor() {}
+  constructor(private productService: ProductService) {}
   pageTitle = 'Latest Products';
   private _listFilter: string = '';
   get listFilter(): string {
@@ -20,51 +21,11 @@ export class ProductListComponent implements OnInit {
   imageWidth = 50;
   showImage = false;
   imageMargin = 2;
-  products: IProduct[] = [
-    {
-      id: 1,
-      name: 'Leaf Rake',
-      code: 'GDN-0011',
-      releaseDate: 'March 19, 2021',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      rating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      id: 2,
-      name: 'Garden Cart',
-      code: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      rating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      id: 5,
-      name: 'Hammer',
-      code: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      rating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-    {
-      id: 8,
-      name: 'Saw',
-      code: 'TBX-0022',
-      releaseDate: 'May 15, 2021',
-      description: '15-inch steel blade hand saw',
-      price: 11.55,
-      rating: 3.7,
-      imageUrl: 'assets/images/saw.png',
-    },
-  ];
 
+  products: IProduct[] = [];
   filteredProducts: IProduct[] = [];
   ngOnInit(): void {
+    this.products = this.productService.getProducts();
     this.filteredProducts = this.products;
   }
   performFilter(filterBy: string): IProduct[] {
